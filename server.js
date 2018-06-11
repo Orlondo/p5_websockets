aff89383f13033ca5da3291763224f2f8d36ecdc
Orlondo <o.rue@sbcglobal.net>
2018-06-11 00:52:59 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+const server = app.listen('3000');
+
+app.use(express.static('public'));
+
+console.log('Server is running...');
+
+const io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection' + socket.id);
+    
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        // io.broadcast.emit('mouse', data);
+        console.log(data)
+    }
+}
+
+
